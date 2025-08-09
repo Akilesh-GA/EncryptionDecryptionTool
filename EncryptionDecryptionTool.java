988acf2e849dcce95824d8765d343e6085bc776c
Akilesh-GA <akileshga.1466@gmail.com>
2025-08-09 12:40:55 +0530

    feat : atbash and xor classes updated

--- EncryptionDecryptionTool.java
+++ EncryptionDecryptionTool.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 interface CeaserCipher {
     public void ceaserCipherMethod();
-    public void getText(String text,int key);
+    public void setText(String text,int key);
     public String encryptText();
     public String decryptText();
 }
@@ -12,16 +12,16 @@ class CeaserCipherImpl implements CeaserCipher {
     int key;
     String res;
     public void ceaserCipherMethod() {
-        System.out.println("\n======================================");
-        System.out.println("         Caesar Cipher Method");
-        System.out.println("======================================\n");
+        System.out.println("\n===========================================");
+        System.out.println("\tCEASER CIPHER TECHNIQUE");
+        System.out.println("===========================================\n");
         
         System.out.println("The Caesar Cipher is one of the encryption algorithm and ancient cryptography method.");
         System.out.println("It works by applying a uniform shift to each alphabetic character in the input string.");
         System.out.println();
     }
 
-    public void getText(String text,int key) {
+    public void setText(String text,int key) {
         this.text = text;
         this.key = key;
     }
@@ -62,7 +62,7 @@ class CeaserCipherImpl implements CeaserCipher {
 
 interface AffineCipher {
     public void affineCipherMethod();
-    public void getText(String text,int key1,int key2);
+    public void setText(String text,int key1,int key2);
     public String encryptText();
     public String decryptText();
 }
@@ -73,15 +73,15 @@ class AffineCipherImpl implements AffineCipher {
     char decryptedChar;
     
     public void affineCipherMethod() {
-        System.out.println("\n====================================");
-        System.out.println("Affine Cipher Method");
-        System.out.println("====================================\n");
+        System.out.println("\n===========================================");
+        System.out.println("\tAFFINE CIPHER TECHNIQUE");
+        System.out.println("===========================================\n");
         System.out.println("The Affine Cipher is a classical substitution using mathematical functions to encrypt and decrypt messages.");
         System.out.println("This technique introduces a more complex level of encryption by combining arithmatic operations.");
         System.out.println();
     }
 
-    public void getText(String text,int key1,int key2) {
+    public void setText(String text,int key1,int key2) {
         this.text = text;
         this.key1 = key1;
         this.key2 = key2;
@@ -138,55 +138,115 @@ class AffineCipherImpl implements AffineCipher {
 }
 
 interface AtbashCipher {
-    default void atbashCipherMethod() {
-        System.out.println();
-        System.out.println("Atbash Cipher Encoding Technique");
-        System.out.println();
-        System.out.println("The Atbash Cipher is a classical substitution cipher where each letter in the alphabet is reversed.\nIt does not use any keys, making it one of the simplest ciphers to implement and understand and Ideal for beginners in cryptography.");
-        System.out.println();
-    }
+    public void atbashCipherMethod();
 }
 
 class AtbashCipherImpl implements AtbashCipher {
-    // No additional methods needed
+    String text;
+
+    public void atbashCipherMethod() {
+        System.out.println("\n===========================================");
+        System.out.println("\tATBASH CIPHER TECHNIQUE");
+        System.out.println("===========================================\n");
+        System.out.println();
+        System.out.println("The Atbash Cipher is a classical substitution cipher where each letter in the alphabet is reversed.");
+        System.out.println("It does not use any keys, making it one of the simplest ciphers to implement and understand and Ideal for beginners in cryptography.");
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
+    public String encryptText() {
+        StringBuffer encryptedString = new StringBuffer();
+        HashMap<Character,Character> map = new HashMap<>();
+        for(char i='a',j='z'; i<='z'; i++,j--) {
+            map.put(i,j);
+        }
+        for(char i='A',j='Z'; i<='Z'; i++,j--) {
+            map.put(i,j);
+        }
+        for(int i=0;i<text.length();i++) {
+            Character mapped = map.get(text.charAt(i));
+            if(mapped != null) {
+                encryptedString.append(mapped);
+            }
+            else {
+                encryptedString.append(text.charAt(i));
+            }
+        }
+        return encryptedString.toString();
+    }
+    public String decryptText() {
+        StringBuffer decryptedString = new StringBuffer();
+        HashMap<Character,Character> map = new HashMap<>();
+        for(char i='z',j='a'; i>='a'; i--,j++) {
+            map.put(i,j);
+        }
+        for(char i='Z',j='A'; i>='A'; i--,j++) {
+            map.put(i,j);
+        }
+        for(int i=0;i<text.length();i++) {
+            Character mapped = map.get(text.charAt(i));
+            if(mapped != null) {
+                decryptedString.append(mapped);
+            }
+            else {
+                decryptedString.append(text.charAt(i));
+            }
+        }
+        return decryptedString.toString();
+    }
 }
 
 
 interface XORCipher {
-    default void XORCipherMethod() {
-        System.out.println();
-        System.out.println("XOR Cipher Encoding Technique");
-        System.out.println();
-        System.out.println("The XOR Cipher is a symmetric encryption technique that applies the XOR (exclusive OR) operation between the plaintext and a key.\nThis makes it a lightweight encryption method commonly used in low-level security systems or embedded devices.");
-        System.out.println();
-    }
+    public void XORCipherMethod();
+    public void setText(String text, char key);
+    public String encryptText();
+    public String decryptText();
 }
 
 class XORCipherImpl implements XORCipher {
-    // No additional methods needed
-}
+    String text;
+    char key;
 
-interface TranspositionCipher {
-    default void transpositionCipherMethod() {
-        System.out.println();
-        System.out.println("Transposition Cipher Encoding Technique");
-        System.out.println();
-        System.out.println("The Transposition Cipher does not alter the characters but rearranges their order based on a fixed system or pattern.\nOften used in puzzle encryption, or in combination with substitution ciphers for stronger results.");
+    public void XORCipherMethod() {
+        System.out.println("\n===========================================");
+        System.out.println("\tXOR CIPHER TECHNIQUE");
+        System.out.println("===========================================\n");
+        System.out.println("The XOR Cipher is a symmetric encryption technique that applies the XOR operation between the plaintext and a key.");
+        System.out.println("This makes it a lightweight encryption method commonly used in low-level security systems or embedded devices.");
         System.out.println();
     }
-}
-
-class TranspositionCipherImpl implements TranspositionCipher {
-    // No additional methods needed
+    public void setText(String text, char key) {
+        this.text = text;
+        this.key = key;
+    }
+    public String encryptText() {
+        StringBuffer encryptedString = new StringBuffer();
+        for(int i=0;i<text.length();i++) {
+            encryptedString.append((char)(text.charAt(i) ^ key));
+        }
+        return encryptedString.toString();
+    }
+    public String decryptText() {
+        StringBuffer decryptedString = new StringBuffer();
+        for(int i=0;i<text.length();i++) {
+            decryptedString.append((char)(text.charAt(i) ^ key));
+        }
+        return decryptedString.toString();
+    }
 }
 
 public class EncryptionDecryptionTool {
     public static void main(String[] args) {
-        System.out.println("Welcome to Encryption and Decryption Tool");
+        System.out.println("\n===========================================");
+        System.out.println("\tENCRYPTION DECRYPTION TOOL");
         int option = 0;
         int choice = 0;
         Scanner scan = new Scanner(System.in);
         do {
+            System.out.println("===========================================\n");
+
             System.out.println("Choose Encrypting / Decrypting Technique");
             System.out.println("1 -> Ceaser Cipher");
             System.out.println("2 -> Affine Cipher");
@@ -196,9 +256,10 @@ public class EncryptionDecryptionTool {
             System.out.println("-1 -> Quit application");
 
             System.out.print("Enter your option : ");
+
             option = scan.nextInt();
 
-            if(option < 1 || option > 5) {
+            if((option < 1 && option != -1) || (option > 5 && option != 1)) {
                 System.out.println("Invalid Input Passed !");
                 System.out.println("Enter option range (1 - 5) ");
             }
@@ -210,6 +271,7 @@ public class EncryptionDecryptionTool {
                     case 1:
                         CeaserCipherImpl cc = new CeaserCipherImpl();
                         do {
+                            System.out.println("\n\n===========================================\n");
                             System.out.println("Ceaser Cipher Method : ");
                             System.out.println();
                             System.out.println("1 -> Description");
@@ -225,22 +287,22 @@ public class EncryptionDecryptionTool {
                                     cc.ceaserCipherMethod();
                                 }
                                 else if(choice == 2) {
-                                    System.out.println("Enter Text to Encrypt : ");
+                                    System.out.print("Enter Text to Encrypt : ");
                                     scan.nextLine();
                                     String text = scan.nextLine();
-                                    System.out.println("Enter Key to Encrypt : ");
+                                    System.out.print("Enter Key to Encrypt : ");
                                     int key = scan.nextInt();
-                                    cc.getText(text, key);
-                                    System.out.println(cc.encryptText());
+                                    cc.setText(text, key);
+                                    System.out.print("Encrypted Text : " + cc.encryptText());
                                 }
                                 else if(choice == 3) {
-                                    System.out.println("Enter Text to Decrypt : ");
+                                    System.out.print("Enter Text to Decrypt : ");
                                     scan.nextLine();
                                     String text = scan.nextLine();
-                                    System.out.println("Enter Key to Decrypt : ");
+                                    System.out.print("Enter Key to Decrypt : ");
                                     int key = scan.nextInt();
-                                    cc.getText(text, key);
-                                    System.out.println(cc.decryptText());
+                                    cc.setText(text, key);
+                                    System.out.print("Decrypted Text : " + cc.decryptText());
                                 }
                             }
                             catch(InputMismatchException e) {
@@ -257,6 +319,7 @@ public class EncryptionDecryptionTool {
                         AffineCipherImpl ac = new AffineCipherImpl();
 
                         do {
+                            System.out.println("\n\n===========================================\n");
                             System.out.println("Affine Cipher Method : ");
                             System.out.println();
                             System.out.println("1 -> Description");
@@ -280,7 +343,7 @@ public class EncryptionDecryptionTool {
                                     int key1 = scan.nextInt();
                                     System.out.println("Enter b value : ");
                                     int key2 = scan.nextInt();
-                                    ac.getText(text, key1,key2);
+                                    ac.setText(text, key1,key2);
                                     System.out.println(ac.encryptText());
                                 }
                                 else if(choice == 3) {
@@ -292,7 +355,7 @@ public class EncryptionDecryptionTool {
                                     int key1 = scan.nextInt();
                                     System.out.println("Enter b value : ");
                                     int key2 = scan.nextInt();
-                                    ac.getText(text, key1, key2);
+                                    ac.setText(text, key1, key2);
                                     System.out.println(ac.decryptText());
                                 }
                             }
@@ -308,15 +371,91 @@ public class EncryptionDecryptionTool {
                         break;
                     case 3:
                         AtbashCipherImpl at = new AtbashCipherImpl();
-                        at.atbashCipherMethod();
+                        do {
+                            System.out.println("\n\n===========================================\n");
+                            System.out.println("Atbash Cipher Method : ");
+                            System.out.println();
+                            System.out.println("1 -> Description");
+                            System.out.println("2 -> Atbash Text - Encryption");
+                            System.out.println("3 -> Atbash Text - Decryption");
+                            System.out.println("-1 -> Quit Atbash Cipher Method");
+
+                            System.out.print("Enter your choice : ");
+                            choice = scan.nextInt();
+
+                            try {
+                                if(choice == 1) {
+                                    at.atbashCipherMethod();
+                                }
+                                else if(choice == 2) {
+                                    System.out.println("Enter Text to Encrypt : ");
+                                    scan.nextLine();
+                                    String text = scan.nextLine();
+                                    at.setText(text);
+                                    System.out.print("Encrypted Text : " + at.encryptText());
+                                }
+                                else if(choice == 3) {
+                                    System.out.println("Enter Text to Decrypt : ");
+                                    scan.nextLine();
+                                    String text = scan.nextLine();
+                                    at.setText(text);
+                                    System.out.print("Decrypted Text : " + at.decryptText());
+                                }
+                            }
+                            catch(InputMismatchException e) {
+                                System.err.println("Invlid Input Passed | Enter input range between (1 - 3)");
+                            }
+                            catch(Exception e) {
+                                System.err.println(e.getMessage());
+                            }
+
+                        }while(choice != -1);
                         break;
                     case 4:
                         XORCipherImpl xor = new XORCipherImpl();
-                        xor.XORCipherMethod(); 
-                        break;
-                    case 5:
-                        TranspositionCipherImpl tc = new TranspositionCipherImpl();
-                        tc.transpositionCipherMethod();
+                        do {
+                            System.out.println("\n===========================================\n");
+                            System.out.println("XOR Cipher Method : ");
+                            System.out.println();
+                            System.out.println("1 -> Description");
+                            System.out.println("2 -> XOR Text - Encryption");
+                            System.out.println("3 -> XOR Text - Decryption");
+                            System.out.println("-1 -> Quit XOR Cipher Method");
+
+                            System.out.print("Enter your choice : ");
+                            choice = scan.nextInt();
+
+                            try {
+                                if(choice == 1) {
+                                    xor.XORCipherMethod();
+                                }
+                                else if(choice == 2) {
+                                    System.out.println("Enter Text to Encrypt : ");
+                                    scan.nextLine();
+                                    String text = scan.nextLine();
+                                    System.out.println("Enter Key to Encrypt : ");
+                                    char key = scan.next().charAt(0);
+                                    xor.setText(text,key);
+                                    System.out.print("Encrypted Text : " + xor.encryptText());
+                                }
+                                else if(choice == 3) {
+                                    System.out.println("Enter Text to Decrypt : ");
+                                    scan.nextLine();
+                                    String text = scan.nextLine();
+                                    System.out.println("Enter Key to Encrypt : ");
+                                    char key = scan.next().charAt(0);
+                                    xor.setText(text,key);
+                                    System.out.print("Decrypted Text : " + xor.decryptText());
+                                }
+                            }
+                            catch(InputMismatchException e) {
+                                System.err.println("Invlid Input Passed | Enter input range between (1 - 3)");
+                            }
+                            catch(Exception e) {
+                                System.err.println(e.getMessage());
+                            }
+
+                        }while(choice != -1);
                         break;
                     default:
                         break;
